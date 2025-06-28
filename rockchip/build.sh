255757680fe76b39a9c048337bf1201cdedbc312
Evelye <104082062+Evelye@users.noreply.github.com>
2025-06-28 20:43:26 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -38,7 +38,7 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-app-mosdns"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
