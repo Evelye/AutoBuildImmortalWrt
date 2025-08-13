2d8d1f34539480d813804d654eb25c658d8f5675
Evelye <104082062+Evelye@users.noreply.github.com>
2025-08-13 21:03:32 +0800

    Update build23.sh

--- rockchip/build23.sh
+++ rockchip/build23.sh
@@ -59,7 +59,6 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 代理工具
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
